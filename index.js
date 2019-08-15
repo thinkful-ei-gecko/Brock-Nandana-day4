b25aaa5afd1df1025bcdc782008bb061c6f3796f
Brock Boutwell <brockboutwell@gmail.com>
2019-08-15 16:10:16 -0400

    cat carousel attempt

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 /*eslint-env jquery*/
 'use strict';
-$('.thumbnail').on('click', e => {
+/*$('.thumbnail').on('click', e => {
   //console.log(e.target);
   $('').replacewith($('.thumbnail'));
-});
+});*/
+
+
+function clickHandler(){
+
+  $('.thumbnail').click(function (event) {
+    const imgSrc = $(this).find('img').attr('src');
+    const imgAlt = $(this).find('img').attr('alt');
+
+  $('.hero img').attr('src', imgSrc);
+  $('.hero img').attr('alt', imgAlt);
+  });
+}
+$(clickHandler);
